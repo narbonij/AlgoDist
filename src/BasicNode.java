0fb203f0ba2a0dd36953e247299ff3a7d23df0fd
fabienjacques <fabien.jacques@etu.u-bordeaux.fr>
2018-11-30 17:30:30 +0100

    La première ronde fonctionne (c'était pas trop compliqué car pour la première ronde tous les fragments sont des sommets seuls). Maintentant il faut adapter ça pour que ça marche avec des fragments de plusieurs sommets).

--- src/BasicNode.java
+++ src/BasicNode.java
@@ -1,6 +1,8 @@
 import jbotsim.Message;
 import jbotsim.Node;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import jbotsim.Color;
@@ -11,29 +13,50 @@ import jbotsim.Color;
  * Chaque sommet à pour label le nombre de messages reçus
  */
 public class BasicNode extends Node {
-	boolean rootMode; //Si rootMode = true, le sommet et la racine de son fragment
 	BasicNode father; //Le père du sommet
-	int d;
+	HashSet<BasicNode> children;
 	
 	@Override
 	public void onStart() {
 		super.disableWireless();
-		rootMode = true;
 		father = null;
-		d = 0;
+		children = new HashSet<BasicNode>() ;
+		
+		//On cherche le sommet le plus proche
+		Distance closestDist = findClosestDistOutsideOfFragment();
+		
+		//Si on est le plus vieux, on devient le père sinon on devient le fils
+		if(closestDist.isOlder()) {
+			sendToDist(closestDist, new Message("IAMDAD"));
+			if(getNodeFromDist(closestDist) != null)
+				children.add(getNodeFromDist(closestDist));
+		}
+		else {
+			sendToDist(closestDist, new Message("YOUAREDAD"));
+			if(getNodeFromDist(closestDist) != null)
+				father = getNodeFromDist(closestDist);
+		}
 	}
 	
 	@Override
 	public void onClock() {
-		sendToDist(findClosestDist(), new Message("hello"));
-		this.setLabel(d);
-		d = 0;
+		updateLabel();
 	}
 	
 	@Override
-	public void onMessage(Message message) {
-		if(message.getContent() == "hello")
-			d++;
+	public void onMessage(Message m) {
+		switch ((String)m.getContent()) {
+		case "IAMDAD":
+			father = (BasicNode) m.getSender();
+			break;
+			
+		case "YOUAREDAD":
+			children.add((BasicNode) m.getSender());
+			break;
+
+		default:
+			break;
+		}
 	}
 	
 	@Override
@@ -41,8 +64,8 @@ public class BasicNode extends Node {
 		
 	}
 	
-	//Cherche la distance au voisin le plus proche
-	public Distance findClosestDist() {
+	//Cherche la distance au voisin le plus proche qui n'est pas dans le fragment
+	private Distance findClosestDistOutsideOfFragment() {
 		List<Node> N = getNeighbors();
 		Distance closestDist = null;
 		if (!N.isEmpty())
@@ -51,21 +74,53 @@ public class BasicNode extends Node {
 					Integer.min(this.getID(), N.get(0).getID()),
 					Integer.max(this.getID(), N.get(0).getID()));			
 			
+		//Recherche du plus petit parmi les voisins
 		for(Node v : N) {
+			if(children.contains(v))
+				continue;
 			Distance distToV = new Distance(getInLinkFrom(v).getLength(), 
 					Integer.min(this.getID(), v.getID()),
 					Integer.max(this.getID(), v.getID()));
 			if (distToV.compareTo(closestDist) == -1)
 				closestDist = distToV;
 		}
+		
+		//Set invader
+		if (closestDist.getMaxID() == this.getID())
+			closestDist.setOlder(true);
+		else
+			closestDist.setOlder(false);
+		
 		return closestDist;
 	}
+	
+	/*
+	 * Renvoie le sommet voisin correspondant à objet Distance
+	 * Renvoie null si le sommet est introuvable
+	 */
+	private BasicNode getNodeFromDist(Distance dist) {
+		List<Node> N = getNeighbors();
+		if (N.isEmpty())
+			return  null;
+		
+		for(Node v : N) {
+			Distance distToV = new Distance(getInLinkFrom(v).getLength(), 
+					Integer.min(this.getID(), v.getID()),
+					Integer.max(this.getID(), v.getID()));
+			if (distToV.compareTo(dist) == 0) {
+				return (BasicNode) v;
+			}
+		}
+		
+		return null;
+	}
+	
 	
 	/*  Cherche parmi les voisins s'il y en a un à distance dist
 	 *  et envoie le message m à ce voisin s'il le trouve
 	 *  renvoie vrai si le sommet est trouvé et faux sinon
 	 */ 
-	public boolean sendToDist(Distance dist, Message m) {
+	private boolean sendToDist(Distance dist, Message m) {
 		List<Node> N = getNeighbors();
 		if (N.isEmpty())
 			return  false;
@@ -81,5 +136,21 @@ public class BasicNode extends Node {
 		}
 		
 		return false;
+	}
+	
+	//Met à jour les infos affichées dans le label du sommet
+	private void updateLabel() {
+		String label = "";
+		label += "Self : " + Integer.toString(getID()) + "  ";
+		if(father != null)
+			label += "Father : " + Integer.toString(father.getID()) + "  ";
+		else
+			label += "Father :   ";
+		label += "Children : ";
+		for (BasicNode v : children) {
+			label += Integer.toString(v.getID()) + " ";
+		}
+		
+		setLabel(label);
 	}
 }
--- src/Distance.java
+++ src/Distance.java
@@ -1,13 +1,27 @@
 
 public class Distance implements Comparable {
-	double distance;
-	int minID;
-	int maxID;
+	private double distance; //distance euclidienne au sommet
+	private int minID; // plus petit ID des deux sommets
+	private int maxID; // plus grand ID des deux sommets
+	private boolean older; //en cas de merge est-ce que mon fragment va devenir le père de l'autre (est-ce que le sommet de plus grand ID est dans mon fragment ?)
+	
     public Distance(double distance, int minID, int maxID) {
 		this.distance = distance;
 		this.minID = minID;
 		this.maxID = maxID;
 	}
+    
+    public boolean isOlder() {
+    	return older;
+    }
+    
+    public void setOlder(boolean invader) {
+    	this.older = invader;
+    }
+    
+    public int getMaxID() {
+    	return maxID;
+    }
 	
 	@Override
 	public int compareTo(Object arg0) {
