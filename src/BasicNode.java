3f3ec1dcef26796665e19155d482666939b44401
jnarboni <jonathan.narboni@etu.u-bordeaux.fr>
2018-12-10 01:14:51 +0100

    Algo ok: tested for multiple topology types

--- src/BasicNode.java
+++ src/BasicNode.java
@@ -1,10 +1,15 @@
 import jbotsim.Message;
 import jbotsim.Node;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.FileHandler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import jbotsim.Color;
 import jbotsim.Link;
@@ -30,12 +35,20 @@ public class BasicNode extends Node
 	private Message futureNew;
 	Set<Pair<Node, Integer>> listMerge;
 	Set<Message> listAckFragToSend;
+	private int nbMess;
 
 	public BasicNode()
 	{
 		super();
 
+		init();
+
+	}
+	
+	private void init()
+	{
 		father = null;
+		setWirelessStatus(true);
 		this.setColor(Color.RED);
 		children = new HashSet<>();
 		listMerge = new HashSet<>();
@@ -51,7 +64,11 @@ public class BasicNode extends Node
 		root = false;
 		hasMerged = false;
 		futureNew = null;
-
+		nbMess = 0;
+		
+		
+		
+		
 	}
 
 	@Override
@@ -76,13 +93,16 @@ public class BasicNode extends Node
 		// System.out.println("node:" + getID() + " get PULSE message");
 		// when receiving message pulse (from father) we increment the phase (reset all
 		// var that has to be) and forward the message
+		init();
+		
+		
 		phase++;
-		sendAckFrag();
 		frag = getID();
+		if(getID()==0)
+			Log("----------START OF ALGORITHM-------------",false);
 		setLabel(getID() + ":" + frag);
-		father = null;
-		// setWirelessStatus(false);
 		reset();
+	
 
 		// it sends a message frag to know the minimum cost outgoing (from the fragment)
 		// edge
@@ -92,58 +112,16 @@ public class BasicNode extends Node
 		}
 
 	}
-
-	@Override
-	public void onClock()
+	
+	private void checkCouterACK_FRAGCounterOUT()
 	{
-		/*
-		 * updateLabel(); if(father != null) setColor(father.getColor());
-		 */
-		if (!hasMerged && ready && listMerge.size() != 0)
-		{
-			// check if in the merge list there s a vertex u s.t. the edge (this,u)=out &&
-			// this.frag<u.frag
-			System.out.println(
-					"-----------checking for merge:" + getID() + " father:" + father + " ---------------------");
-			/*
-			 * if(father == null)//this is the new root of its fragment {
-			 */
-
-			for (Pair<Node, Integer> p : listMerge)
-			{
-				children.add(p.first());
-				if(father == null)
-				{
-					if (out.getOtherEndpoint(this).equals(p.first()) && frag < p.second())
-					{
-						System.out.println(
-								"------------------node:" + getID() + " becomes the new root.--------------------");
-						System.out.println("--------------MERGING FRAG:" + frag + " AND FRAG:" + p.second());
-						root = true;
-						hasMerged = true;
-					}
-				}
-			}
-
-			if (root)
-			{
-				for (Node n : children)
-				{
-					System.out.println("sending to child:" + n);
-					send(n, new Message(frag, "NEW"));
-
-				}
-			}
-			listMerge.clear();
-			ready = false;
-			// }
-		} else if (counterAckFragNeighbors == getNeighbors().size() && counterChildrenOut == children.size())
+		if (counterAckFragNeighbors == getNeighbors().size() && counterChildrenOut == children.size())
 		{
 			counterAckFragNeighbors = 0;
 			counterChildrenOut = 0;
-			System.out.println(
+			/*System.out.println(
 					"all " + children.size() + " children and all neighbors of node:" + getID() + " answered.");
-			System.out.println("OUT=" + out);
+			System.out.println("OUT=" + out);*/
 			// as soon as we receive the OUT from all children and frag from other
 			// neighbors, we forward the computed out value to the father
 			if (father != null)
@@ -154,6 +132,8 @@ public class BasicNode extends Node
 				if (out == null)
 				{
 					System.out.println("--------------------Algo Terminted in " + phase + " phases---------------------");
+					//send(this,new Message(null,"STOP"));
+					//getTopology().pause();
 					for(Node n : children)
 					{
 						send(n,new Message(null,"STOP"));
@@ -178,10 +158,12 @@ public class BasicNode extends Node
 						father = outNode;
 						this.setColor(DEFAULT_COLOR);
 						children.remove(outNode);
-						System.out.println("changing father of:" + getID() + " into:" + outNode);
+						//System.out.println("changing father of:" + getID() + " into:" + outNode);
 					} else
 					{
-						System.out.println("sending merge");
+						father = null;
+						this.setColor(Color.RED);
+						//System.out.println("sending merge");
 						send(out.getOtherEndpoint(this), new Message(frag, "MERGE"));
 					}
 					ready = true;
@@ -190,6 +172,71 @@ public class BasicNode extends Node
 			}
 
 		} 
+	}
+
+	@Override
+	public void onClock()
+	{
+		/*
+		 * updateLabel(); if(father != null) setColor(father.getColor());
+		 */
+		if (!hasMerged && ready && listMerge.size() != 0)
+		{
+			// check if in the merge list there s a vertex u s.t. the edge (this,u)=out &&
+			// this.frag<u.frag
+			//System.out.println(
+					//"-----------checking for merge:" + getID() + " father:" + father + " ---------------------");
+			/*
+			 * if(father == null)//this is the new root of its fragment {
+			 */
+
+			for (Pair<Node, Integer> p : listMerge)
+			{
+				children.add(p.first());
+				if(father == null)
+				{
+					if (out.getOtherEndpoint(this).equals(p.first()) && frag < p.second())
+					{
+						/*System.out.println(
+								"------------------node:" + getID() + " becomes the new root.--------------------");
+						System.out.println("--------------MERGING FRAG:" + frag + " AND FRAG:" + p.second());*/
+						root = true;
+						hasMerged = true;
+					}
+				}
+			}
+
+			if (root)
+			{
+				if(father == null && out != null)
+				{
+					out.setColor(Color.BLUE);
+				}
+				for (Node n : children)
+				{
+					//System.out.println("sending to child:" + n);
+					send(n, new Message(frag, "NEW"));
+
+				}
+			}
+			listMerge.clear();
+			ready = false;
+			
+		} //else 
+		else if (hasMerged && listMerge.size() !=0)
+		{
+			for (Pair<Node, Integer> p : listMerge)
+			{
+				if(p.first() != father)
+				{
+					children.add(p.first());
+					send(p.first(),new Message(frag, "NEW"));
+				}
+			}
+			listMerge.clear();
+			
+			
+		}
 		else if (!hasMerged && futureNew != null)
 		{
 			frag = (Integer) futureNew.getContent();
@@ -225,7 +272,8 @@ public class BasicNode extends Node
 		 * 
 		 * default: break; }
 		 */
-		 System.out.println("PHASE:" + phase + "::" + "NODE:" + getID() + " from frag:" + frag + " with father:" + father + " received message:" + m.getFlag() + " from sender:" + m.getSender());
+		nbMess++;
+		 Log("PHASE:" + phase + "::" + "NODE:" + getID() + " from frag:" + frag + " with father:" + father + " and " + children.size() + " children"+ " RECEIVE message:" + m.getFlag() + " from sender:" + m.getSender());
 
 		switch (m.getFlag())
 		{
@@ -237,7 +285,7 @@ public class BasicNode extends Node
 			reset();
 			for (Node n : children)
 			{
-				System.out.println("sending PULSE to child:" + n);
+				//System.out.println("sending PULSE to child:" + n);
 				send(n, new Message(null,"PULSE"));
 			}
 			// moreover it send a message frag to know the minimum cost outgoing (from the
@@ -245,12 +293,13 @@ public class BasicNode extends Node
 			for (Node n : getNeighbors())
 			{
 				send(n, new Message(phase, "FRAG"));
+				//System.out.println("PHASE:" + phase + "::" + "NODE:" + getID() + " from frag:" + frag + " with father:" + father + " send FRAG message to:" + n );
 			}
 			break;
 
 		case "FRAG":
 			// just answer with your frag number
-			if (phase == (Integer) m.getContent())
+			if (phase >= (Integer) m.getContent())
 			{
 				send(m.getSender(), new Message(frag, "ACK_FRAG"));
 			} else
@@ -263,31 +312,36 @@ public class BasicNode extends Node
 
 		case "ACK_FRAG":
 			// if we receive a frag message from a vertex of another fragment
+			counterAckFragNeighbors++;
+			Log(counterAckFragNeighbors + " on " + getNeighbors().size() + "ACK_FRAG RECEIVED");
 			if ((Integer) m.getContent() != frag)
 			{
 				// if it is the first, we take it as min
 				if (out == null)
 				{
 					out = getCommonLinkWith(m.getSender());
-					System.out.println("first out found:" + out);
+					//System.out.println("first out found:" + out);
 					outNode = null;
 				} 
 				else // else we take it if the distance is smaller than the previous out
 				{
 					if (Distance.smaller(getCommonLinkWith(m.getSender()), out))
 					{
-						System.out.println("new OUT=" + getCommonLinkWith(m.getSender()) + ":"
-								+ getCommonLinkWith(m.getSender()).getLength() + " is smaller than " + out + ":"
-								+ out.getLength());
+						//System.out.println("new OUT=" + getCommonLinkWith(m.getSender()) + ":"
+								//+ getCommonLinkWith(m.getSender()).getLength() + " is smaller than " + out + ":"
+								//+ out.getLength());
 						out = getCommonLinkWith(m.getSender());
 						outNode = null;
 					}
 				}
 			}
-			counterAckFragNeighbors++;
+			checkCouterACK_FRAGCounterOUT();
+			
 			break;
 
 		case "OUT":
+			counterChildrenOut++;
+			Log(counterChildrenOut + " on " + children.size() + " OUT RECEIVED");
 			// when we receive an potential outgoing edge from sons: check and replace out
 			// if smaller
 			if (m.getContent() != null)
@@ -303,6 +357,7 @@ public class BasicNode extends Node
 					outNode = (BasicNode) m.getSender();
 				}
 			}
+			checkCouterACK_FRAGCounterOUT();
 			/*
 			 * else { if(father == null) {
 			 * 
@@ -311,7 +366,7 @@ public class BasicNode extends Node
 			 * 
 			 * } else { send(father, m); } }
 			 */
-			counterChildrenOut++;
+			
 			break;
 
 		case "ACK_NEW_ROOT":
@@ -321,29 +376,23 @@ public class BasicNode extends Node
 			{
 				send(n, new Message (m.getContent(),m.getFlag()));
 			}
-			if (out != null)
+			if (out != null && ((Link) m.getContent()).equals(out))
 			{
-
-				if (((Link) m.getContent()).equals(out))
+				if (outNode != null)
 				{
-					if (this != out.endpoint(0) && this != out.endpoint(1))
-					{
-						System.out.println("changing father of:" + getID() + " into:" + outNode);
-						children.add(father);
-						children.remove(outNode);
-						father = outNode;
-						this.setColor(DEFAULT_COLOR);
+					//System.out.println("changing father of:" + getID() + " into:" + outNode);
+					children.add(m.getSender());
+					children.remove(outNode);
+					father = outNode;
+					this.setColor(DEFAULT_COLOR);
+				} else // you are the new root
+				{
+					children.add(father);
+					father = null;
+					this.setColor(Color.RED);
+					//System.out.println("---------------" + getID() + " becomes new ROOT");
 
-						// send(outNode,m);
-					} else // you are the new root
-					{
-						children.add(father);
-						father = null;
-						this.setColor(Color.RED);
-						System.out.println("---------------" + getID() + " becomes new ROOT");
-
-						send(out.getOtherEndpoint(this), new Message(frag, "MERGE"));
-					}
+					send(out.getOtherEndpoint(this), new Message(frag, "MERGE"));
 				}
 			}
 
@@ -352,12 +401,15 @@ public class BasicNode extends Node
 
 		case "MERGE":
 		{
+			
+			
 			if (!hasMerged)
 			{
-				System.out.println("adding:" + m.getSender() + " to list merge");
+				//System.out.println("adding:" + m.getSender() + " to list merge");
 				listMerge.add(new Pair<Node, Integer>(m.getSender(), (Integer) m.getContent()));
 			} else
 			{
+				children.add(m.getSender());
 				send(m.getSender(), new Message(frag, "NEW"));
 			}
 
@@ -366,13 +418,17 @@ public class BasicNode extends Node
 
 		case "NEW":
 
-			if (ready /*true*/)
+			if (true)
 			{
-				System.out.println("Message from sender:" + m.getSender().getID());
-				System.out.println("________________Putting node:" + getID() + " from frag:" + frag +  " into frag:" + m.getContent() + "______");
+				//System.out.println("Message from sender:" + m.getSender().getID());
+				//System.out.println("________________Putting node:" + getID() + " from frag:" + frag +  " into frag:" + m.getContent() + "______");
 				frag = (Integer) m.getContent();
 				setLabel(getID() + ":" + frag);
 				hasMerged = true;
+				if(father == null && out != null)
+				{
+					out.setColor(Color.BLUE);
+				}
 				father = (BasicNode) m.getSender();
 				this.setColor(DEFAULT_COLOR);
 				children.remove(father);// only needed for the other vertex that compose the loop of length 1 that
@@ -396,8 +452,9 @@ public class BasicNode extends Node
 
 		case "ECHO":
 			counterEcho++;
-			System.out.println("message ECHO:" + getID() + " with father:" + father + " from node:" + m.getSender().getID());
-			if (counterEcho == children.size())
+			Log(counterEcho + " on " + children.size() + " ECHO messages RECEIVED");
+			//System.out.println("message ECHO:" + getID() + " with father:" + father + " from node:" + m.getSender().getID());
+			if (counterEcho >= children.size())
 			{
 				counterEcho = 0;
 				if (father != null)
@@ -408,7 +465,7 @@ public class BasicNode extends Node
 				{
 					phase++;
 					sendAckFrag();
-					System.out.println("----------------PHASE:" + phase + " from node:" + getID() + "-------------------");
+					//System.out.println("----------------PHASE:" + phase + " from node:" + getID() + "-------------------");
 					reset();
 
 					for (Node n : children)
@@ -546,17 +603,19 @@ public class BasicNode extends Node
 		hasMerged = false;
 		out = null;
 		outNode = null;
-		/*counterAckFragNeighbors = 0;
+		counterAckFragNeighbors = 0;
 		counterChildrenOut = 0;
-		counterEcho = 0;*/
+		counterEcho = 0;
 		//futureNew = null;
 		//listMerge.clear();
-		System.out.println("______________RESET OF NODE:" + getID() + " of FRAG:" + frag + " with father:" + father + " _______________");
+		
+		//System.out.println("______________RESET OF NODE:" + getID() + " of FRAG:" + frag + " with father:" + father + " _______________");
 		for(Node n : children)
 		{
-			System.out.println("with child:" + n.getID());
+			//System.out.println("with child:" + n.getID());
 		}
-		System.out.println("______________END::PHASE: " + phase + " ______________");
+		//System.out.println("______________END::PHASE: " + (phase-1) + " ______________");
+		
 	}
 
 	private void sendAckFrag()
@@ -575,4 +634,64 @@ public class BasicNode extends Node
 		listAckFragToSend.removeAll(fragMessToRemove);
 		//listAckFragToSend.clear();
 	}
+	
+	
+	
+	@Override
+	public void send(Node n, Message mess)
+	{
+		// TODO Auto-generated method stub
+		Log("PHASE:" + phase + "::" + "NODE:" + getID() + " from frag:" + frag + " with father:" + father + " and " + children.size() + " children" + " SEND message:" + mess.getFlag() + " to node:" + n.getID());
+		super.send(n,mess);
+	}
+	
+	private void Log(String s)
+	{
+		Log(s,true);
+	}
+
+	private void Log(String s, boolean append)
+	{
+		Logger logger = Logger.getLogger("MyLog");  
+		logger.setUseParentHandlers(false);
+	
+	    FileHandler fh;  
+
+	    try {  
+
+	        // This block configure the logger with handler and formatter  
+	        fh = new FileHandler("./log.txt",append); 
+	        logger.addHandler(fh);
+	        fh.setFormatter(new SimpleFormatter() {
+	            private static final String format = "%1$s %n";
+
+	            @Override
+	            public synchronized String format(LogRecord lr) {
+	                return String.format(format,
+	                        lr.getMessage()
+	                );
+	            }
+	        }); 
+
+	        // the following statement is used to log any messages  
+	        logger.info(s);  
+	        logger.removeHandler(fh);
+	        fh.close();
+
+	    } catch (SecurityException e) {  
+	        e.printStackTrace();  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    }  
+	    
+	    
+
+		
+	}
+
+	public int getNbMess()
+	{
+		return nbMess;
+	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,96 @@
+import java.nio.file.LinkOption;
+
+import jbotsim.Color;
+import jbotsim.Link;
+import jbotsim.Message;
+import jbotsim.Node;
 import jbotsim.Topology;
+import jbotsim.event.MessageListener;
+import jbotsim.event.SelectionListener;
+import jbotsim.event.TopologyListener;
 import jbotsimx.topology.TopologyGenerator;
 import jbotsimx.ui.JViewer;
 
 public class Main{
     public static void main(String[] args){
         Topology tp = new Topology();
-        //tp.setWirelessStatus(false);
         tp.setDefaultNodeModel(BasicNode.class);
-        new JViewer(tp);
+        final Integer[] nbMessTotal = new Integer[1];
+        final Integer[] nbStopedNode = new Integer[1];
+        nbMessTotal[0] = 0;
+        nbStopedNode[0] = 0;
+   
         
-        TopologyGenerator.generateRing(tp, 31);
-        //Link l = new
+        JViewer viewer = new JViewer(tp);
+        
+
+        
+        TopologyGenerator.generateRing(tp, 5);
+        
+        //viewer.onCommand(command);
+
+        tp.addTopologyListener(new TopologyListener()
+		{
+			
+			@Override
+			public void onNodeRemoved(Node arg0)
+			{
+				tp.resetTime();
+				for(Link l : tp.getLinks())
+				{
+					
+					l.setColor(Link.DEFAULT_COLOR);
+					l.setWidth(1);
+				}
+				tp.restart();
+				tp.start();
+				
+			}
+			
+			@Override
+			public void onNodeAdded(Node n)
+			{
+				for(Link l : tp.getLinks())
+				{
+					
+					l.setColor(Link.DEFAULT_COLOR);
+					l.setWidth(1);
+				
+			
+					
+				}
+					
+			}
+		});
+        
+        tp.addMessageListener(new MessageListener()
+		{
+			
+			@Override
+			public void onMessage(Message mess)
+			{
+				
+				if(mess.getFlag() == "STOP")
+				{
+					nbMessTotal[0] += ((BasicNode)mess.getSender()).getNbMess();
+					nbStopedNode[0]++;
+					if(nbStopedNode[0] == (tp.getNodes().size()-1))
+					{
+						for(Link l : tp.getLinks())
+						{
+							if(l.getColor() == Color.BLUE)
+							{
+								l.setColor(Color.MAGENTA);
+							}
+						}
+						System.out.println("With " + nbMessTotal[0] + " messages sent.");
+					}
+					
+				}
+				
+			}
+		});
+       
         tp.setClockSpeed(50);
         tp.start();
       
